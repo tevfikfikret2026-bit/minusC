76de8ea8897c272217ec02aba519ec42a2eb27b3
μC <tevfik.fikret2026@gmail.com>
2026-05-16 21:44:12 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,28 +1,51 @@
 /*
- * src/main.c - Written completely in Minus-C syntax
+ * src/main.c - Comprehensive evaluation of Minus-C syntax
  */
 
 #include "minusc.h"
 
-// Ana fonksiyon girişi
-i32 main(v_d) {
-    
-    // Veri tipleri artık u8 ve u32
-    u8 counter = 0;
-    u32 total_sum = 0;
+// Bir struct (str) yapısını Minus-C ile tanımlıyoruz
+tdef str {
+    u8  id;
+    u16 score;
+    bln active;
+} player_t;
 
-    // whl (while) döngüsü kullanımı
-    whl (counter < 100) {
+// Dosya kapsamlı (stc) sabit (cst) veri dizisi
+stc cst u8 Max_Limit = 200;
+
+i32 main(v_d) {
+    // Değişkenler ultra kısa veri tipleriyle kuruluyor
+    player_t p1;
+    p1.id = 1;
+    p1.score = 500;
+    p1.active = 1;
+
+    u32 total = 0;
+    
+    // _for döngüsü mimarisi
+    _for (u8 i = 0; i < Max_Limit; i++) {
         
-        // _if (if) kontrolü
-        _if (counter == 50) {
-            brk; // Döngüden çık (break)
-        }
+        // _if / _ei / _el mimarisi
+        _if (i == 50) {
+            cnt; // pas geç (continue)
+        } 
+        _ei (i == 150) {
+            brk; // döngüyü kır (break)
+        } 
         _el {
-            total_sum += counter;
+            total += i;
         }
-        
-        counter++;
+    }
+
+    // Swc (switch-case) mimarisi
+    swc (p1.id) {
+        cse 1:
+            p1.score += 100;
+            brk;
+        dfl:
+            p1.score = 0;
+            brk;
     }
 
     rtn 0; // Geri dönüş (return)
